f7592c4c22283e94755f902ea1cd84d7db438d48
jawadzd <jawad.zein.deen@hotmail.com>
2024-07-07 16:25:33 +0300

    movede all the code of the details component to the files and added the import lib

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit,Input} from '@angular/core';
+import { HeroesComponent } from "../heroes/heroes.component";
+import { HEROES } from '../mock-heroes';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../heroes/hero';
 
 @Component({
   selector: 'app-hero-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule,HeroesComponent],
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.css'
 })
 export class HeroDetailComponent {
+
+  @Input() hero?: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
