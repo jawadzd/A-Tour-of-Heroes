738669b68a3c758f7b7ba694f596605eb65d738d
jawadzd <jawad.zein.deen@hotmail.com>
2024-07-07 23:20:55 +0300

    fixing the code of app-routing.modules and removing unneeded imports

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 // src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
-  // Define your routes here
+
+    {path: 'heroes', component: HeroesComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from "./heroes/heroes.component";
+import { AppRoutingModule } from './app-routing.module';
 import { MessagesComponent } from './messages/messages.component';
 
 
@@ -9,7 +10,7 @@ import { MessagesComponent } from './messages/messages.component';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet,MessagesComponent, HeroesComponent]
+    imports: [RouterOutlet,MessagesComponent, HeroesComponent,AppRoutingModule]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
